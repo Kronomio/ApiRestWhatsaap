87779d5ec97b6b4cbe0fd8ac81e869bd12af846e
Cristian López <cristianlp.ok@gmail.com>
2024-08-21 20:15:46 -0300

    Correcion de errores

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,31 @@
-import {createBot, createProvider, createFlow, addKeyword, MemoryDB} from '@bot-whatsapp/bot'
-import {BaileysProvider, handleCtx} from '@bot-whatsapp/provider-baileys'
+import { createBot, createProvider, createFlow, addKeyword, MemoryDB } from '@bot-whatsapp/bot';
+import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys';
 
+const flowBienvenida = addKeyword('hola').addAnswer('¡Buenas! ¡Bienvenido!');
 
-const flowBienvenida=addKeyword('hola').addAnswer('Buenas!! Bienvenido')
-const main=async () => {
-    const adapterDB = new MemoryDB()
-    const provider=createProvider(BaileysProvider)
+const main = async () => {
+    const adapterDB = new MemoryDB();
+    const provider = createProvider(BaileysProvider);
+
     
-    provider.initHttpServer(3002)
 
-    provider.http?.server.post('/send-message', handleCtx(async (bot, req, res)=>{
-        const body =req.body
-        const message = body.message
-        const mediaUrl=body.mediaUrl
-        
-        await bot.sendMessage('5493541653254', message,{
-            media:mediaUrl
-        } )
-        res.end('etos es un msj de polka')
-    })
-    )
+    provider.initHttpServer(3002);
+
+    if (provider.http?.server) {
+        provider.http.server.post('/send-message', handleCtx(async (bot, req, res) => {
+            const { message, mediaUrl, phone } = req.body;
+            await bot.sendMessage(phone, message, {
+                media: mediaUrl,
+            });
+            res.end('Este es un mensaje de polka');
+        }));
+    }
+
     await createBot({
-        flow:createFlow([flowBienvenida]),
-        database:adapterDB,
-        provider
-    })
+        flow: createFlow([flowBienvenida]),
+        database: adapterDB,
+        provider,
+    });
+};
 
-
-}
-
-main()
+main();
